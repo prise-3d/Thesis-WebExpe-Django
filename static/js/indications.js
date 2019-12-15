b475c638495d8e08a95fedd517b81b29989e99cb
Vasiliki <vmyrodia@gmail.com>
2019-12-16 00:31:57 +0100

    random choice of scenes and limited number of scenes for each experiment

--- static/js/indications.js
+++ static/js/indications.js
@@ -6,12 +6,16 @@ const KEYCODE_RIGHT_ARROW = 39
 
 urlParams = new URLSearchParams(window.location.search)
 
-const scene    = urlParams.get('scene')
+var scene      = urlParams.get('scene')
 const expe     = urlParams.get('expe')
 var example    = urlParams.get('example')
 
-if (example === null || example === ''){
+if (example == null || example == ''){
      example = 0
+}
+
+if (scene === null || scene === ''){
+     scene = document.getElementsByName('scene_name')[0].value
 }
 
 const checkKey = e => {
@@ -28,11 +32,11 @@ const checkKey = e => {
           window.location = expeUrl + params
      }
      else if (e.keyCode === KEYCODE_LEFT_ARROW || e.keyCode === KEYCODE_RIGHT_ARROW) {
-          
           // increment number of example
           example = parseInt(example) + 1
 
           console.log("I'm here")
+          
           // construct url with params for experiments
           const params = `?scene=${scene}&expe=${expe}&example=${example}`
           window.location = indicationsUrl + params
