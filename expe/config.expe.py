7f6a3832375833795e1d13fcf47634c358f88f25
Vasiliki <vmyrodia@gmail.com>
2020-03-09 23:07:46 +0100

    change indications (after feedback), add threshold as parameters in config

--- expe/config.expe.py
+++ expe/config.expe.py
@@ -32,25 +32,25 @@ expes_configuration            = {
     'quest_one_image':{
         # do not forget to add slopes and new scenes is added for this experiment
         'scenes':['contemporary', 'bathroom'], 
-        'expected_duration': 20,
+        'expected_duration': 30,
         'text':{
-            'presentation' : "The computer generated images are widely used nowadays. \n We have designed an experiment which will allow you to find out at which level you are capable of detecting the aberration in a scene.",
-            'question': "Do you see one image with the same quality everywhere or a composition of more than one image with different qualities?",
+            'presentation' : "The computer generated images are widely used nowadays. \n We have designed an experiment that will allow you to find out at which level you are capable of detecting the aberration in a scene. \n \n The question that you have to answer is if you see one image with the same quality everywhere or  composition of more than one image with different qualities. \n \n The images were cut horizontally or vertically (always side to side) and merged with an image from a different or same quality.\n It can't be a square in the middle of the image, it is always from one end to the other.",
+            'question': "Do you see one image with the same quality everywhere or composition of more than one image with different qualities? \n \n Please be aware that we do not ask you to detect if the image is blurred. All the images are blurred more or less and this is part of the difficulty of the task.",
             'indication': "press LEFT is you see the same quality, RIGHT if not",
             'end_text': "Experience is finished. Thanks for your participation!",
             'examples': {
                 'sentence': ["Let's see some examples! \n Example 1 from 6 : ", 
-                             "The answer is a composition with different qualities! \n This image is cropped {0}.\n {1}% on the left originating from a low-quality image and on the right originating from high quality. \n So, press RIGHT.",
+                             "The answer is composition with different qualities! \n This image is cropped {0}.\n {1}% on the left originating from a low-quality image and on the right originating from high quality. \n So, press RIGHT.",
                              "Example 2 from 6 : ",
-                             "The answer is the same quality! \n This image is cropped {0} but there is not any difference in the quality because \n {1}% on the left originating from a high-quality image and on the right originating from the same quality.\n So, press LEFT.", 
-                             "Example 3 from 6: ",
-                             "The answer is a composition with different qualities! \n This image is cropped {0}. \n {1}% on the upper part originating from a low-quality image and on the bottom originating from high quality. \n So, press RIGHT.",
-                             "Example 4 from 6: ",
-                             "The answer is a composition with different qualities! \n This image is cropped {0}. \n {1}% on the upper part originating from a low-quality image and on the bottom originating from high quality. \n So, press RIGHT.",
-                             "Example 5 from 6: ",
-                             "The answer is a composition with different qualities! \n This image is cropped {0}. \n {1}% on the upper part originating from a high-quality image and on the bottom originating from a low-quality quality image.\n So, press RIGHT.",
-                             "Example 6 from 6: ",
-                             "Even if it is more difficult to detect the difference the answer is a composition with different qualities! \n This image is cropped {0}. \n {1}% on the left originating from a low-quality image and on the right originating from a high quality image. \n So, press RIGHT.",
+                             "The answer is the same quality! \n This image is cropped {0} but there is not any difference in the quality because \n {1}% on the left originating from a high-quality image and on the right originating from the same quality.\n Please be aware that even if the image is blurred, there is not a difference in the blur level! \n So, press LEFT.", 
+                             "Example 3 from 6 : ",
+                             "The answer is composition with different qualities! \n This image is cropped {0}. \n {1}% on the upper part originating from a low-quality image and on the bottom originating from high quality. \n So, press RIGHT.",
+                             "Example 4 from 6 : ",
+                             "The answer is composition with different qualities! \n This image is cropped {0}. \n {1}% on the upper part originating from a low-quality image and on the bottom originating from high quality. \n So, press RIGHT.",
+                             "Example 5 from 6 : ",
+                             "The answer is composition with different qualities! \n This image is cropped {0}. \n {1}% on the upper part originating from a high-quality image and on the bottom originating from a low-quality quality image.\n So, press RIGHT.",
+                             "Example 6 from 6 : ",
+                             "Even if it is more difficult to detect the difference the answer is composition with different qualities! \n Pay attention to the details ! \n This image is cropped {0}. \n {1}% on the left originating from a low-quality image and on the right originating from a high quality image.\n  \n So, press RIGHT.",
                              ],
                 'crop_params': [
                     [0.3, 0, 0],
@@ -71,22 +71,26 @@ expes_configuration            = {
                     [1, -1],
                     [-1, -1],
                     [-1, -1],
-                    [7, -1],
-                    [7, -1],
                     [15, -1],
                     [15, -1],
-                    [20, -1],
-                    [20, -1],
-                    [30, -1],
-                    [30, -1]
+                    [25, -1],
+                    [25, -1],
+                    [40, -1],
+                    [40, -1],
+                    [50, -1],
+                    [50, -1]
                 ]
             }
         },
         'params':{
             'min_iterations': 150,
-            'max_iterations': 250,
-            'max_time': 60, #minutes
+            'max_iterations': 200,
+            'max_time': 90, #minutes
             'entropy': 0.2,
+            'thresholds':{
+                'contemporary' : [50, 20050, 50], #start, stop (non inclusive), step
+                'bathroom' : [10, 2010, 10], #start, stop, step
+            },
             'slopes':{
                 'contemporary' : [0.0001, 0.001, 0.00003], #start, stop, step
                 'bathroom' : [0.0005, 0.01, 0.0003], #start, stop, step
